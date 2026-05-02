02b9d0d2690ade8f27f191ac4ede40680f183e69
vivannkhanna <vivanngkhanna1@gmail.com>
2026-05-02 13:25:53 -0400

    phone fix again

--- src/Home.jsx
+++ src/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
           onClick={() => navigate("/interviews")}
           className="
             absolute 
-            top-[30%] left-[10%] 
+            top-[30%] left-[12%] 
             sm:top-[32%] sm:left-[13%]
             lg:top-[34%] lg:left-[14%]
 
@@ -44,7 +44,7 @@ export default function Home() {
           onClick={() => navigate("/about")}
           className="
             absolute 
-            top-[33%] left-[32%]
+            top-[33%] left-[34%]
             sm:top-[35%] sm:left-[35%]
             lg:top-[37%] lg:left-[37%]
 
@@ -63,7 +63,7 @@ export default function Home() {
           onClick={() => window.open("https://instagram.com", "_blank")}
           className="
             absolute 
-            top-[31%] left-[44%]
+            top-[30.5%] left-[46%]
             sm:top-[33%] sm:left-[47%]
             lg:top-[34.5%] lg:left-[48.5%]
 
@@ -82,7 +82,7 @@ export default function Home() {
           onClick={() => window.open("https://tiktok.com", "_blank")}
           className="
             absolute 
-            top-[36%] left-[58%]
+            top-[37%] left-[63%]
             sm:top-[39%] sm:left-[62%]
             lg:top-[41.5%] lg:left-[64.5%]
 
@@ -101,7 +101,7 @@ export default function Home() {
           onClick={() => navigate("/articles")}
           className="
             absolute 
-            top-[29%] left-[75%]
+            top-[29.5%] left-[81%]
             sm:top-[31%] sm:left-[80%]
             lg:top-[33.25%] lg:left-[83%]
 
